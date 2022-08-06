42f44d274aecb24e9800d53e966ffb96e0f3f547
Terabithia1572 <legendys65@gmail.com>
2022-08-06 23:23:38 +0300

    Project Created

--- Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/HomeController.cs
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace Asp.NetCore5._0_Dynamic_Blog_Project.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
     }
 }
