47a8c2cb45411d20132aa696b93e210b923ad2aa
Terabithia1572 <legendys65@gmail.com>
2022-08-07 18:57:39 +0300

    Category Interface/Added

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,29 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        //burada 4 metodumuz olacak bu methodlardan
+        // 3 ü Void olarak tutulacak (Create Update Delete)
+        // 1 tanesi ise List olarak tutulacak (Read) yani Listele
+        List<Category> ListAllCategory(); // Kategorinin tümünü
+                                          // getir demek
+        void AddCategory(Category category);
+        void DeleteCategory(Category category);
+        void UpdateCategory(Category category);
+
+        Category GetByID(int id); // güncelleme yapmak için
+                                    //bir id'ye göre güncelleme yapacağız
+                                    // o yüzden ID getirmemiz için bu methodu tanımladık
+        
+
+
+
+    }
+}
