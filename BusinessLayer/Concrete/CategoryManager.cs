82922468cb7c98e8a8cbaadd0a23d467624927bc
Terabithia1572 <legendys65@gmail.com>
2022-08-08 21:12:54 +0300

    Fill in the CategoryManager

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -10,29 +11,32 @@ namespace BusinessLayer.Concrete
 {
     public class CategoryManager : ICategoryService
     {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
         public void CategoryAdd(Category category)
         {
-            throw new NotImplementedException();
+            repo.Insert(category);
         }
 
         public void CategoryDelete(Category category)
         {
-            throw new NotImplementedException();
+            if(category.CategoryID !=0) { 
+            repo.Delete(category);
+            }
         }
 
         public void CategoryUpdate(Category category)
         {
-            throw new NotImplementedException();
+            repo.Update(category);
         }
 
         public Category GetByID(int id)
         {
-            throw new NotImplementedException();
+            return repo.GetByID(id);
         }
 
         public List<Category> GetList()
         {
-            throw new NotImplementedException();
+            return repo.GetListAll();
         }
     }
 }
