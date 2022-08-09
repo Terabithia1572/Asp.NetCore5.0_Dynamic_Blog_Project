472ae84073333bf302ba0a3181d39130e6e605df
Terabithia1572 <legendys65@gmail.com>
2022-08-09 19:09:05 +0300

    Abstract Updated+EntityFramework/Added

--- DataAccessLayer/Abstract/ICategoryDal.cs
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -7,20 +7,20 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface ICategoryDal
+    public interface ICategoryDal:IGenericDal<Category>
     {
         //burada 4 metodumuz olacak bu methodlardan
         // 3 ü Void olarak tutulacak (Create Update Delete)
         // 1 tanesi ise List olarak tutulacak (Read) yani Listele
-        List<Category> ListAllCategory(); // Kategorinin tümünü
-                                          // getir demek
-        void AddCategory(Category category);
-        void DeleteCategory(Category category);
-        void UpdateCategory(Category category);
+        //List<Category> ListAllCategory(); // Kategorinin tümünü
+        //                                  // getir demek
+        //void AddCategory(Category category);
+        //void DeleteCategory(Category category);
+        //void UpdateCategory(Category category);
 
-        Category GetByID(int id); // güncelleme yapmak için
-                                    //bir id'ye göre güncelleme yapacağız
-                                    // o yüzden ID getirmemiz için bu methodu tanımladık
+        //Category GetByID(int id); // güncelleme yapmak için
+        //                            //bir id'ye göre güncelleme yapacağız
+        //                            // o yüzden ID getirmemiz için bu methodu tanımladık
         
 
 
