88ad9c70a1442f262f1ea3f9df0254205c194da3
Terabithia1572 <legendys65@gmail.com>
2022-08-14 14:29:04 +0300

    ViewComponents/Added

---
+++ Asp.NetCore5.0_Dynamic_Blog_Project/ViewComponents/CommentList.cs
@@ -0,0 +1,32 @@
+﻿using Asp.NetCore5._0_Dynamic_Blog_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Dynamic_Blog_Project.ViewComponents
+{
+    public class CommentList:ViewComponent
+    {
+        //Invoke Çağır demek
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID=1,
+                    UserName="Yunus İNAN"
+
+                },
+                new UserComment
+                {
+                    ID=2,
+                    UserName="Faruk Erdemiş"
+                }
+            };
+            return View(commentvalues);
+        }
+    }
+}
