384bb6d886c9be0f4bec6b92bd24d181771778cb
Terabithia1572 <legendys65@gmail.com>
2022-08-15 23:52:44 +0300

    RegisterController/Added

--- Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/RegisterController.cs
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/RegisterController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +11,19 @@ namespace Asp.NetCore5._0_Dynamic_Blog_Project.Controllers
 {
     public class RegisterController : Controller
     {
+        WriterManager writerManager = new WriterManager(new EfWriterRepository());
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Index()
+        public IActionResult Index(Writer p)
         {
-            return View();
+            p.WriterStatus = true;
+            p.WriterAbout = "Deneme Test";
+            writerManager.WriterAdd(p);
+            return RedirectToAction("Index","Blog");
         }
     }
 }
