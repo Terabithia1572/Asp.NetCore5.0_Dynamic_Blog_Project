4a22884ca168e1cca40c5b4e3f2271c98b1c5bd0
Terabithia1572 <legendys65@gmail.com>
2022-08-17 20:14:21 +0300

    RegisterController Fluent Validation  / Added

--- Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/RegisterController.cs
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/RegisterController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -20,10 +22,25 @@ namespace Asp.NetCore5._0_Dynamic_Blog_Project.Controllers
         [HttpPost]
         public IActionResult Index(Writer p)
         {
-            p.WriterStatus = true;
-            p.WriterAbout = "Deneme Test";
-            writerManager.WriterAdd(p);
-            return RedirectToAction("Index","Blog");
+            WriterValidator _writerValidator = new WriterValidator(); // Fluent Validation Result seçiyoruz
+            ValidationResult result = _writerValidator.Validate(p); //sonuc adında bir parametre oluşturduk
+            // p den gelen değğerler doğruysa
+            if(result.IsValid) //eger ki sonuçlar geçerli ise o zaman süslü parantez içindekileri yap
+            {
+                p.WriterStatus = true;
+                p.WriterAbout = "Deneme Test";
+                writerManager.WriterAdd(p);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+           
         }
     }
 }
