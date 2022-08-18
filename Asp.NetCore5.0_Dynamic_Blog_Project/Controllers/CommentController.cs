c32b0df7b75e835f27d70bfbe40e4e23b8570926
Terabithia1572 <legendys65@gmail.com>
2022-08-18 21:13:52 +0300

    Comment Page / Updated

--- Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/CommentController.cs
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/CommentController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -15,10 +16,21 @@ namespace Asp.NetCore5._0_Dynamic_Blog_Project.Controllers
         {
             return View();
         }
+        [HttpGet]
         public PartialViewResult PartialAddComment()
         {
             return PartialView();
         }
+        [HttpPost]
+        public PartialViewResult PartialAddComment(Comment p)
+        {
+            p.CommentDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.CommentStatus = true;
+            p.BlogID = 2;
+            commentManager.CommentAdd(p);
+            return PartialView();
+        }
+
         public PartialViewResult CommentListByBlog(int id)
         {
             var values=commentManager.GetList(id);
