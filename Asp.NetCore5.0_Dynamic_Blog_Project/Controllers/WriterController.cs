a97202f2877b5aa78126f0bf6b9fd2b901d59e36
Terabithia1572 <legendys65@gmail.com>
2022-08-27 20:20:53 +0300

    Authorize / Added

---
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/WriterController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Dynamic_Blog_Project.Controllers
+{
+    public class WriterController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+    }
+}
