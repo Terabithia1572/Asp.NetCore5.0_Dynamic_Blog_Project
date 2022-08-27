9d7c952085b17a610f7ba9e4d71a48aab4a13826
Terabithia1572 <legendys65@gmail.com>
2022-08-27 16:40:35 +0300

    AboutController / Updated

--- Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/AboutController.cs
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/AboutController.cs
@@ -13,12 +13,13 @@ namespace Asp.NetCore5._0_Dynamic_Blog_Project.Controllers
         AboutManager aboutManager = new AboutManager(new EfAboutRepository());
         public IActionResult Index()
         {
-            return View();
+            var values = aboutManager.GetList();
+            return View(values);
         }
         public PartialViewResult SocialMediaAbout()
         {
-            var values = aboutManager.GetList();
-            return PartialView(values);
+            
+            return PartialView();
         }
     }
 }
