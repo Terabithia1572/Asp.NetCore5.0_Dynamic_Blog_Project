0d5d451393f2bf178fe304ce1f9ff5d88f3a6f50
Terabithia1572 <legendys65@gmail.com>
2022-08-30 19:33:51 +0300

    Refactoring Process / +

--- BusinessLayer/Abstract/IBlogService.cs
+++ BusinessLayer/Abstract/IBlogService.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Abstract
 {
-    public interface IBlogService
+    public interface IBlogService:IGenericService<Blog>
     {
-        void BlogAdd(Blog blog);
-        void BlogDelete(Blog blog);
-        void BlogUpdate(Blog blog);
-        List<Blog> GetList();
-        Blog GetByID(int id);
+        //void BlogAdd(Blog blog);
+        //void BlogDelete(Blog blog);
+        //void BlogUpdate(Blog blog);
+        //List<Blog> GetList();
+        //Blog GetByID(int id);
         List<Blog> GetBlogListWithCategory();
         List<Blog> GetBlogListByWriter(int id);
         
