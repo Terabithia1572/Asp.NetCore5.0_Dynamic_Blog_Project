8779813fc02324ab09a0f525213e0b3e76c74135
Terabithia1572 <legendys65@gmail.com>
2022-09-04 18:56:55 +0300

    Dashboard Controller / Updated

--- Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/DashboardController.cs
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/DashboardController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,9 +12,17 @@ namespace Asp.NetCore5._0_Dynamic_Blog_Project.Controllers
 {
     public class DashboardController : Controller
     {
+
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        Context c = new Context();
+
         [AllowAnonymous]
         public IActionResult Index()
         {
+            ViewBag.ToplamBlogSayisi = blogManager.GetList().Count();
+            ViewBag.YazarinBlogSayisi = blogManager.GetBlogListByWriter(3).Count();
+            ViewBag.KategoriSayisi = categoryManager.GetList().Count();
             return View();
         }
     }
