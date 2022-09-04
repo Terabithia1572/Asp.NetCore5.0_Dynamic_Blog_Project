7dc9b0366f98fcbd274ddad8a84589fe9a2a7992
Terabithia1572 <legendys65@gmail.com>
2022-09-04 21:24:47 +0300

    Notification / Added

---
+++ Asp.NetCore5.0_Dynamic_Blog_Project/ViewComponents/Notification/NotificationList.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Dynamic_Blog_Project.ViewComponents.Notification
+{
+    public class NotificationList:ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = blogManager.GetLast3Blog();
+            return View(values);
+        }
+    }
+}
