bc04ef5253d2e6a1fd1595f6880d83114327f12e
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-09-28 00:00:16 +0300

    Admin Theme / Added

---
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore5._0_Dynamic_Blog_Project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
