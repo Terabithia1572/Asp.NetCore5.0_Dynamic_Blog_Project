040643bef01823b674476afe759a482a46711637
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-09-27 22:04:36 +0300

    Message Details List / Added

---
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/MessageController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore5._0_Dynamic_Blog_Project.Controllers
+{
+    [AllowAnonymous]
+    public class MessageController : Controller
+    {
+        Message2Manager messageManager = new Message2Manager(new EfMessage2Repository());
+
+        public IActionResult InBox()
+        {
+            int id = 2;
+            var values = messageManager.GetInboxListByWriter(id);
+            return View(values);
+        }
+
+        
+        public IActionResult MessageDetails(int id)
+        {
+            var value = messageManager.TGetByID(id);
+
+            return View(value);
+        }
+    }
+}
