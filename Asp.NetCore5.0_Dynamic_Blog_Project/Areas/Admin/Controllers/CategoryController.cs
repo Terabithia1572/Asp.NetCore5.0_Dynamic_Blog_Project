a7b5ce43caa7278dfd5d1eba2571d5cdd831c2cc
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-09-29 22:45:55 +0300

    Pagination / Added

--- Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/Controllers/CategoryController.cs
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.Controllers
 {
@@ -12,9 +13,9 @@ namespace Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index()
+        public IActionResult Index(int page=1)
         {
-            var values = categoryManager.GetList();
+            var values = categoryManager.GetList().ToPagedList(page,3);
             return View(values);
         }
     }
