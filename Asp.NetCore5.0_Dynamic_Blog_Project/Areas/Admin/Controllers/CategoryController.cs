e801d18f58cacd4c92345e3a1c4455273d9cab62
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-09-29 23:12:02 +0300

    Admin Panel New Category Process/ Added

--- Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/Controllers/CategoryController.cs
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -17,6 +20,37 @@ namespace Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.Controllers
         {
             var values = categoryManager.GetList().ToPagedList(page,3);
             return View(values);
+        }
+        [HttpGet]
+
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            CategoryValidator _categoryValidator = new CategoryValidator(); // Fluent Validation Result seçiyoruz
+            ValidationResult result = _categoryValidator.Validate(category); //sonuc adında bir parametre oluşturduk
+
+
+            // p den gelen değğerler doğruysa
+            if (result.IsValid) //eger ki sonuçlar geçerli ise o zaman süslü parantez içindekileri yap
+            {
+                category.CategoryStatus = true;
+                
+                
+                categoryManager.TAdd(category);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
