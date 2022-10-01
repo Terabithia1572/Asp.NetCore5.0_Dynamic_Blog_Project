1c27ef2cfe2d29a69e5b2f0c16dc44cde4d97f8f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-02 00:05:49 +0300

    Admin Table Database / Added

---
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic4:ViewComponent
+    {
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
