fc78e4bdb486ebcee5ad542e7d8f4fa28af9b783
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-01 23:39:14 +0300

    Statistic 2 / Added

---
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2:ViewComponent
+    {
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+           // ViewBag.toplamblogSayisi = blogManager.GetList().Count();
+            ViewBag.sonPost = context.Blogs.OrderByDescending(x=>x.BlogID).Select(x=>x.BlogTitle)
+                .Take(1).FirstOrDefault();
+            ViewBag.toplamyorumSayisi = context.Comments.Count();
+            return View();
+        }
+    }
+}
