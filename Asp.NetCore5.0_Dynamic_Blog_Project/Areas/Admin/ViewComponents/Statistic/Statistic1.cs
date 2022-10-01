1d98d24b770bedc4680e4ad68a281e1710a08223
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-01 23:22:06 +0300

    Statistic / Added

---
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1:ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.toplamblogSayisi = blogManager.GetList().Count();
+            ViewBag.mesajSayisi = context.Contacts.Count();
+            ViewBag.toplamyorumSayisi = context.Comments.Count();
+            return View();
+        }
+    }
+}
