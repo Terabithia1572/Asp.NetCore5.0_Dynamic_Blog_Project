74e5f74f95a23b29319e3bbcc9c4ca40cd77b546
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-02 16:34:39 +0300

    Google Chart Process1 / Added

---
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,39 @@
+﻿using Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.Controllers
+{
+    public class ChartController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> List = new List<CategoryClass>();
+            List.Add(new CategoryClass
+            {
+                CategoryName = "Teknoloji",
+                CategoryCount = 10
+            });
+            List.Add(new CategoryClass
+            {
+                CategoryName = "Yazılım",
+                CategoryCount = 14
+            });
+            List.Add(new CategoryClass
+            {
+                CategoryName = "Spor",
+                CategoryCount = 5
+            });
+            return Json(new { jasonList = List });
+        }
+    }
+}
