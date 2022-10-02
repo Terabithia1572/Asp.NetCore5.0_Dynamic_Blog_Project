ef5cc82ee63fb62ddfff0eae50fd23e8ffdd320f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-02 16:23:56 +0300

    get weather information with api  / Added

--- Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.ViewComponents.Statistic
 {
@@ -18,6 +19,11 @@ namespace Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.ViewComponents.Statis
             ViewBag.toplamblogSayisi = blogManager.GetList().Count();
             ViewBag.mesajSayisi = context.Contacts.Count();
             ViewBag.toplamyorumSayisi = context.Comments.Count();
+            string api = "b75c2ca9e69997207c923bc5666d536c";
+                string connection = "https://api.openweathermap.org/data/2.5/weather?q=van&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.Sicaklik = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
             return View();
         }
     }
