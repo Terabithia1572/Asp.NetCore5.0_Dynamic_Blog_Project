cf5e274cd739a91e6461794b86afe5241362873e
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-04 22:31:32 +0300

    Swagger and Api Get List / Added

---
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using BlogApiDemo.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApiDemo.Controllers
+{
+
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult EmployeeList()
+        {
+            using var context = new Context();
+            var values = context.Employees.ToList();
+            return Ok(values); // api başarılı istek olursa parantez içinde dönecek status kodu yazılıyor.
+        }
+    }
+}
