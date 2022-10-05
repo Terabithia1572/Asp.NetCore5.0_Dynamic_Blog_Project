e8ee0a399abf8e365f5a5942d8d5b1bf338d610a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-06 00:22:22 +0300

    Identity Library / Added

--- DataAccessLayer/Migrations/20221005212001_mig_Identity_add_userApp.Designer.cs
+++ DataAccessLayer/Migrations/20221005212001_mig_Identity_add_userApp.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20221005212001_mig_Identity_add_userApp")]
+    partial class mig_Identity_add_userApp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
