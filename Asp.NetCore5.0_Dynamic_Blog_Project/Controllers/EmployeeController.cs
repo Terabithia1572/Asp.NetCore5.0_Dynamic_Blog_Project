21a55454d7963b4324b3d6887380a62cc6e9b69e
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-05 23:08:07 +0300

    Communication of the project with APIs List & add through the project / Added

---
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/EmployeeController.cs
@@ -0,0 +1,58 @@
+﻿using Asp.NetCore5._0_Dynamic_Blog_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Dynamic_Blog_Project.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //private readonly IHttpClientFactory _httpClient;
+        //public EmployeeController(IHttpClientFactory httpClient)
+        //{
+        //    _httpClient = httpClient;
+        //}
+
+        public async Task<IActionResult> Index()
+        {
+            using (var client = new HttpClient())
+            {
+                var responseMessage = await client.GetAsync("https://localhost:44396/api/Default");
+                var jsonString = await responseMessage.Content.ReadAsStringAsync(); //asenkron olarak karsıla
+                var values = JsonConvert.DeserializeObject<List<EmployeeVM>>(jsonString); //listelerken
+
+                return View(values);
+            }
+        }
+
+
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(EmployeeVM employee)
+        {
+            using (var client = new HttpClient())
+            {
+                var jsonEmployee = JsonConvert.SerializeObject(employee); //eklersen 
+                StringContent content = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+                var responseMessage = await client.PostAsync("https://localhost:44396/api/Default", content);
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(employee);
+            }
+        }
+
+    }
+
+}
