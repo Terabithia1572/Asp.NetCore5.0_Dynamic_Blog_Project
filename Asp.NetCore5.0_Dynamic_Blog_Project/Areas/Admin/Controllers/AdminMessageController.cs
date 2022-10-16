06b2c80a5974edcc2b8310bb4627eceb02616a27
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-16 20:13:48 +0300

    Admin Blog List / Added

--- Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/Controllers/AdminMessageController.cs
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -33,9 +34,24 @@ namespace Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.Controllers
             return View(values);
         }
 
+        [HttpGet]
         public IActionResult ComposeMessage()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult ComposeMessage(Message2 message)
+        {
+            var userName = User.Identity.Name;
+            var userMail = context.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            //sisteme otantike olan kullanıcının bilgilerinin gelmesi
+            var writerId = context.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID).FirstOrDefault();
+            message.SenderID = writerId;
+            message.ReceiverID = 2;
+            message.MessageDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            message.MessageStatus = true;
+            messageManager.TAdd(message);
+            return RedirectToAction("SendBox");
         }
     }
 }
