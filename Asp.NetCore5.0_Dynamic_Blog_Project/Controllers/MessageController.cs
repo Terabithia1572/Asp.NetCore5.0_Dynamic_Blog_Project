98c897d468944b32c35bc955b9638f9b1c35cd77
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-16 14:47:43 +0300

    Completing Message Processes in the Author Panel/Added

--- Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/MessageController.cs
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Controllers/MessageController.cs
@@ -1,13 +1,14 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 
 namespace Asp.NetCore5._0_Dynamic_Blog_Project.Controllers
 {
-    [AllowAnonymous]
     public class MessageController : Controller
     {
         Message2Manager messageManager = new Message2Manager(new EfMessage2Repository());
@@ -22,6 +23,15 @@ namespace Asp.NetCore5._0_Dynamic_Blog_Project.Controllers
             var values = messageManager.GetInboxListByWriter(writerId);
             return View(values);
         }
+        public IActionResult SendBox()
+        {
+            var userName = User.Identity.Name;
+            var userMail = c.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            //sisteme otantike olan kullanıcının bilgilerinin gelmesi
+            var writerId = c.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID).FirstOrDefault();
+            var values = messageManager.GetSendBoxListByWriter(writerId);
+            return View(values);
+        }
 
         
         public IActionResult MessageDetails(int id)
@@ -29,6 +39,25 @@ namespace Asp.NetCore5._0_Dynamic_Blog_Project.Controllers
             var value = messageManager.TGetByID(id);
 
             return View(value);
+        }
+        [HttpGet]
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SendMessage(Message2 message)
+        {
+            var userName = User.Identity.Name;
+            var userMail = c.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            //sisteme otantike olan kullanıcının bilgilerinin gelmesi
+            var writerId = c.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID).FirstOrDefault();
+            message.SenderID = writerId;
+            message.ReceiverID = 2;
+            message.MessageStatus = true;
+            message.MessageDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            messageManager.TAdd(message);
+            return RedirectToAction("Inbox");
         }
     }
 }
