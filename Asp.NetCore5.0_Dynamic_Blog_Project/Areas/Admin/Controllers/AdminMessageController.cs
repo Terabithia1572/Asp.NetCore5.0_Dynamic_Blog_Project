d6f2ed178ae6e3391d05d3d67a5a34fe42daee6a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-16 16:49:17 +0300

    Admin Panel Arrangements and Message Page / Added

---
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/Controllers/AdminMessageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.Controllers
+{
+    public class AdminMessageController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult InBox()
+        {
+            return View();
+        }
+    }
+}
