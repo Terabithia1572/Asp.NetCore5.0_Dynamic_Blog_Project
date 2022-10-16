ca741c4d3e464da5912c94397e62860b56ad7e3a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-16 19:22:11 +0300

    Admin Panel Outbox and New Message Page / Added

--- Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/Controllers/AdminMessageController.cs
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,10 +9,31 @@ using System.Threading.Tasks;
 
 namespace Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class AdminMessageController : Controller
     {
-        [Area("Admin")]
+        Context context = new Context();
+        Message2Manager messageManager = new Message2Manager(new EfMessage2Repository());
         public IActionResult InBox()
+        {
+            var userName = User.Identity.Name;
+            var userMail = context.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            //sisteme otantike olan kullanıcının bilgilerinin gelmesi
+            var writerId = context.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID).FirstOrDefault();
+            var values = messageManager.GetInboxListByWriter(writerId);
+            return View(values);
+        }
+        public IActionResult SendBox()
+        {
+            var userName = User.Identity.Name;
+            var userMail = context.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            //sisteme otantike olan kullanıcının bilgilerinin gelmesi
+            var writerId = context.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID).FirstOrDefault();
+            var values = messageManager.GetSendBoxListByWriter(writerId);
+            return View(values);
+        }
+
+        public IActionResult ComposeMessage()
         {
             return View();
         }
