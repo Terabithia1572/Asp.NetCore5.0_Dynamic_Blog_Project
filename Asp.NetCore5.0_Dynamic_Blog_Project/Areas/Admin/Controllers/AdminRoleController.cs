2c10aeb46b7469d2b0887e85f4972a7edf4ab061
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-17 20:38:37 +0300

    Role List and Add Roles / Added

---
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/Controllers/AdminRoleController.cs
@@ -0,0 +1,56 @@
+﻿using Asp.NetCore5._0_Dynamic_Blog_Project.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminRoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public AdminRoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _roleManager.Roles.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(RoleViewModel roleViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                AppRole role = new AppRole
+                {
+                    Name = roleViewModel.name
+                };
+
+                var result = await _roleManager.CreateAsync(role);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("",item.Description);
+                }
+            }
+            return View(roleViewModel);
+        }
+    }
+}
