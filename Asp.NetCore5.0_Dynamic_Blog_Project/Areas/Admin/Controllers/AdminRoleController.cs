fe12ea4af88d5d380d05398c7528d48cf01481c5
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-17 21:14:26 +0300

    Editing Roles and Deleting Roles / Added

--- Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/Controllers/AdminRoleController.cs
+++ Asp.NetCore5.0_Dynamic_Blog_Project/Areas/Admin/Controllers/AdminRoleController.cs
@@ -1,4 +1,5 @@
-﻿using Asp.NetCore5._0_Dynamic_Blog_Project.Models;
+﻿using Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.Models;
+using Asp.NetCore5._0_Dynamic_Blog_Project.Models;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -51,6 +52,42 @@ namespace Asp.NetCore5._0_Dynamic_Blog_Project.Areas.Admin.Controllers
                 }
             }
             return View(roleViewModel);
+        }
+        [HttpGet]
+        public IActionResult UpdateRole(int id)
+        {
+            var values = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            RoleUpdateViewModel roleUpdateViewModel = new RoleUpdateViewModel
+            {
+                Id = values.Id,
+                name = values.Name
+
+            };
+
+            return View(roleUpdateViewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateRole(RoleUpdateViewModel roleUpdateViewModel)
+        {
+            var values = _roleManager.Roles.Where(x => x.Id == roleUpdateViewModel.Id).FirstOrDefault();
+            values.Name = roleUpdateViewModel.name;
+            var result = await _roleManager.UpdateAsync(values);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(roleUpdateViewModel);
+        }
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var values = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            var result = await _roleManager.DeleteAsync(values);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
